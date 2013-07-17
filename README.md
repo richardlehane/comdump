0046d6145ed4e1b805c8bbfc4d8ed919e28adbf4
Richard Lehane <richard.lehane@gmail.com>
2013-07-17 10:32:48 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Take a quick look at MS compound objects.
+
+Tool based on github.com/richardlehane/mscfb package.
+It creates files for each of the directory entries in an compound object
+and writes them to a comobjects directory.
+
+Example:
+    
+    ./comdump -in test.doc
---
+++ comdump.go
@@ -0,0 +1,97 @@
+// Copyright 2013 Richard Lehane. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Take a quick look at MS compound objects.
+//
+// Tool based on github.com/richardlehane/mscfb package.
+// It creates files for each of the directory entries in an compound object
+// and writes them to a comobjects directory.
+//
+// Example:
+//   ./comdump -in test.doc
+package main
+
+import (
+	"flag"
+	"github.com/richardlehane/mscfb"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"unicode"
+)
+
+var in = flag.String("in", "", "path to input Compound object")
+
+func clean(str string) string {
+	buf := make([]rune, 0, len(str))
+	for _, r := range str {
+		if unicode.IsPrint(r) {
+			buf = append(buf, r)
+		}
+	}
+	return string(buf)
+}
+
+func process(in string) error {
+	file, err := os.Open(in)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	doc, err := mscfb.NewReader(file)
+	if err != nil {
+		return err
+	}
+	dir, base := filepath.Split(in)
+	base = strings.Join(strings.Split(base, "."), "_")
+	base += "_comobjects"
+	path := filepath.Join(dir, base)
+	err = os.Mkdir(path, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	for {
+		entry, err := doc.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		name := clean(entry.Name)
+		outFile, err := os.Create(filepath.Join(path, name))
+		if err != nil {
+			return err
+		}
+		if _, err := io.Copy(outFile, doc); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func main() {
+	flag.Parse()
+	if len(*in) < 1 {
+		log.Fatalln("Missing required argument: -in path_to_compound_object")
+	}
+	err := process(*in)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
